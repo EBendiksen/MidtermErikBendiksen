d805d45de391ef00cd58f51832d1c0eb34927ce7
EBendiksen <71287712+EBendiksen@users.noreply.github.com>
2020-10-05 15:18:37 -0500

    Add files via upload

---
+++ animals.js
@@ -0,0 +1,5 @@
+/* let myAnimals = ['dog', 'cat', 'horse', 'meerkat'];
+      myAnimals.push(prompt("Please enter the name of an animal"));
+  
+var returnAnimal = myAnimals [myAnimals.length - 1];
+  alert(`The last animal is a/an ${returnAnimal}.`); */
---
+++ calculate.js
@@ -0,0 +1,12 @@
+/* let num1 = Number(prompt("enter your first number"));
+let num2 = Number(prompt("enter your second number"));
+let num3 = Number(prompt("enter your third number"));
+
+function calcAverageSquare (num1, num2, num3) {
+  let myAnswer = ((((num1 + num2 + num3)/3) * Math.pow(num1, 2)));
+  return myAnswer;
+}
+
+let result = calcAverageSquare (num1, num2, num3);
+  alert(`The answer is ${result}.`) */
+  
